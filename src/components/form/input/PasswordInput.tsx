7ae7be83690d39bca5526efd597b90ee6832d9e3
Aakash <aakash@zigmaglobal.com>
2026-03-17 16:24:20 +0530

    eye icon aligned

--- src/components/form/input/PasswordInput.tsx
+++ src/components/form/input/PasswordInput.tsx
@@ -37,7 +37,7 @@ export default function PasswordInput({
       <button
         type="button"
         onClick={() => setShowPassword(!showPassword)}
-        className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        className="absolute right-2 inset-y-0 my-auto flex h-full items-center text-gray-500 hover:text-gray-700"
         tabIndex={-1}
       >
         {showPassword ? (
